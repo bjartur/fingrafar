a98cb2c8bc95f1b0d620f967754dc81e6978328d
Pétur Ó. Aðalgeirsson <peturoa1@gmail.com>
2018-10-10 13:39:35 +0000

    save image to file

--- backend/fingerprint_generator.py
+++ backend/fingerprint_generator.py
@@ -1,34 +1,67 @@
 from random import uniform
+from tempfile import mktemp
+from os import path, mkdir
+import time
 
 from pywinauto import Application
 
-app = Application(backend='uia').start('SFinGeDemo/SFinGe.exe')
-main = app.Dialog
+class Generator():
 
-main.Generate.click()
-app.OpenDialog['Select sensor area and resolution'].OK.click()
+    def _init_(self):
+        pass
 
-dialog = main.Dialog
+    def bla(self):
+        self.generate()
+        #fetch image from clipboard
+
+    def generate(self):
+        app = Application(backend='uia').start('SFinGeDemo/SFinGe.exe')
+        main = app.Dialog
+
+        main.Generate.click()
+        app.OpenDialog['Select sensor area and resolution'].OK.click()
+
+        dialog = main.Dialog
 
 
-# def randomize_slider(slider):
-#     slider.set_value(uniform(slider.min_value, slider.max_value))
+        # def randomize_slider(slider):
+        #     slider.set_value(uniform(slider.min_value, slider.max_value))
 
-dialog.Generate.click()
-dialog.Next.click()
-dialog.Next.click()
-dialog.Button5.click() #Start ridge generation
-dialog.Next.click()
+        dialog.Generate.click()
+        dialog.Next.click()
+        dialog.Next.click()
+        dialog.Button5.click() #Start ridge generation
+        dialog.Next.click()
 
-dialog.Next.click()
-dialog.Next.click()
-dialog.Next.click()
+        dialog.Next.click()
+        dialog.Next.click()
+        dialog.Next.click()
 
-dialog.Next.click()
-dialog.Next.click()
-dialog.Next.click()
-dialog.Finish.click()
+        dialog.Next.click()
+        dialog.Next.click()
+        dialog.Next.click()
+        dialog.Finish.click()
 
-main['Copy image to clipboard'].click()
+        full_path = mktemp('.bmp')
+        directory, filename = path.split(full_path)
+        fg_tmp = path.join(directory, 'fingerprint_generator')
+        if not path.exists(fg_tmp):
+            mkdir(fg_tmp)
 
-main.Exit.click()
+        main['Save image to file'].click()
+        save_dialog = main.Dialog
+        save_dialog.child_window(title="File name:", control_type="Edit")\
+            .wait('visible').set_edit_text(fg_tmp)
+        save_dialog.Save.click()
+        time.sleep(1)
+        save_dialog.child_window(title="File name:", control_type="Edit")\
+            .wait('visible').set_edit_text(filename)
+        save_dialog.Save.click()
+        main.Exit.click()
+
+        file = open(path.join(fg_tmp, filename), 'rb')
+
+        return file
+
+if __name__ == '__main__':
+    Generator().generate()
