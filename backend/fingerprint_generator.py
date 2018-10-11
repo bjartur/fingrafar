13ac02ff8d0fa00047770e4a5f9c7828f0780b50
Pétur Ó. Aðalgeirsson <peturoa1@gmail.com>
2018-10-11 12:18:21 +0000

    update project owner requirements

--- backend/fingerprint_generator.py
+++ backend/fingerprint_generator.py
@@ -29,10 +29,11 @@ class Generator():
 
         #Step 1 - Fingerprint mask generation
         form.Generate.click() #generate random mask
-        #TODO: randomize finger selection
+        #TODO: always select thumb
         form.Next.click()
 
         #Step 2 - Directional map generation
+        #TODO: make fingerprint class distribution imitate real distribution among humans
         self.randomize_combobox(form.child_window(
             title='Fingerprint class',
             control_type='ComboBox'
@@ -62,11 +63,13 @@ class Generator():
         form.Next.click()
 
         #Step 5 - Finger contact region
+        #TODO: keep displacement close to 0, both vertical
         self.randomize_slider(form.child_window(
             title='Displacement',
             auto_id='1171', #vertical displacement
             control_type='Slider'
         ))
+        #TODO: and horizontal
         self.randomize_slider(form.child_window(
             auto_id='1170', #horizontal displacement
             control_type='Slider'
@@ -75,6 +78,7 @@ class Generator():
         form.Next.click()
 
         #Step 6 - Pressure/Dryness
+        #TODO: avoid extreme pressure/dryness
         self.randomize_slider(form.child_window(
             auto_id='1104', #pressure/dryness
             control_type='Slider'
@@ -104,6 +108,7 @@ class Generator():
         form.Next.click()
 
         #Step 8 - Noising and rendering
+        #TODO: limit ridge noise to below 50%
         self.randomize_slider(form.child_window(
             title='Ridges', #ridge noise
             control_type='Slider'
@@ -123,15 +128,18 @@ class Generator():
         form.Next.click()
 
         #Step 9 - Fingerprint rotation and translation and apply
+        #TODO: make rotation alwasy 0
         self.randomize_slider(form.child_window(
             title='Rotation',
             control_type='Slider'
         ))
+        #TODO: make translation always 0, both vertical
         self.randomize_slider(form.child_window(
             title='Translation',
             auto_id='1171', #vertical translation
             control_type='Slider'
         ))
+        #TODO: and horizontal
         self.randomize_slider(form.child_window(
             auto_id='1170', #horizontal translation
             control_type='Slider'
@@ -140,18 +148,22 @@ class Generator():
         form.Next.click()
 
         #Step 10 - Background and contrast
+        #TODO: select always Background='None'
         self.randomize_combobox(form.child_window(
             title='Background',
             control_type='ComboBox'
         ), number_of_items=3)
+        #TODO: skip background noise randomization
         self.randomize_slider(form.child_window(
             title='Noise', #background noise
             control_type='Slider'
         ))
+        #TODO: make contrast alwasy 0
         self.randomize_slider(form.child_window(
             title='Contrast',
             control_type='Slider'
         ))
+        #TODO: limit gamma to below 40%
         self.randomize_slider(form.child_window(
             title='Gamma',
             control_type='Slider'
